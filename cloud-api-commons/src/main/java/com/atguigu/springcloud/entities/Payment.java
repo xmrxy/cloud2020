751bd201f9a8729f17ad9b0cfa3ed5985430c5e4
xmrxy <xmrxy@sina>
2020-06-02 23:17:23 +0800

    eureka集群配置好，已测试正常

---
+++ cloud-api-commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private static final long serialVersionUID = 7047338992893173893L;
+    private Long id;
+    private String serial;
+}
---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,43 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Resource
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult<Payment> create(@RequestBody Payment payment) {
+        int i = paymentService.create(payment);
+        log.info("插入结果" + i);
+        if (i > 0) {
+            return new CommonResult(200, "插入数据成功,端口号："+serverPort,i);
+        }else {
+            return new CommonResult(444,"插入数据失败",null);
+        }
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        log.info("获取结果" + payment);
+        if (payment!=null) {
+            return new CommonResult(200, "查询成功，端口号："+serverPort,payment);
+        }else {
+            return new CommonResult(444,"没有对应记录，查询id:"+id,null);
+        }
+    }
+}
